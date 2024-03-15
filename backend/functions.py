5e90e130706d19ec7579a1ada2113ff026542c37
Ashash94 <asanches.devia@gmail.com>
2024-03-15 12:18:19 +0100

    ajout des modeles spectral et dbscan

--- backend/functions.py
+++ backend/functions.py
@@ -1,9 +1,35 @@
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering, DBSCAN
+from sklearn.metrics import silhouette_score
 from config import DATA
 
+def kmeans_silhouette_score():
 
-def kmeans_inertia():
     model = KMeans(n_clusters=5, n_init="auto")
-    model.fit(DATA[["Spending Score (1-100)" , "Annual Income (k$)"]])
+    model.fit(DATA[["Spending Score (1-100)", "Annual Income (k$)"]])
+    
+    labels = model.labels_
+    
+    silhouette_score_avg = silhouette_score(DATA, labels)
+    
+    return silhouette_score_avg
 
-    return model.inertia_
+def spectral_silhouette_score():
+
+    model = SpectralClustering(n_clusters=6, n_init=50)
+    model.fit(DATA[["Spending Score (1-100)", "Annual Income (k$)"]])
+    
+    labels = model.labels_
+    
+    silhouette_score_avg = silhouette_score(DATA, labels)
+    
+    return silhouette_score_avg
+    
+def dbscan_silhouette_score():
+    model = DBSCAN(eps=9, min_samples=3)
+    model.fit(DATA[["Spending Score (1-100)", "Annual Income (k$)"]])
+    
+    labels = model.labels_
+    
+    silhouette_score_avg = silhouette_score(DATA, labels)
+    
+    return silhouette_score_avg
